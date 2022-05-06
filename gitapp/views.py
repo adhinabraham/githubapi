6d8e24978a8475d69d8fa78cfb533a49ffbb7897
adhinabraham <adhinabraham2000@gmail.com>
2022-05-06 19:57:10 +0530

    functionality completed

--- gitapp/views.py
+++ gitapp/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from http.client import HTTPResponse
 import string
 from django.shortcuts import render
@@ -5,20 +6,57 @@ import requests
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 import json
+import io
 
 # Create your views here.
 class github(APIView):
-       
+   
     def post (self,request):
         forksnumber=request.data['forks']
-        self.github_url = r'https://api.github.com/'
-        print('thisi api view ')
+        commites = int(request.data['commites'])
+        
         url=f'https://api.github.com/search/repositories?q=forks:>{forksnumber}+sort:forks-desc'
-        githubdata=requests.get(url)
-        content=githubdata
-        print(content)
-        return Response ({"content":content} )
+        githubdata=json.loads((requests.get(url).text))
+        reporesults = []
+        for repo in githubdata['items']:
+            print(repo['id'])
+            reporesults.append([repo['id'],repo['name'],repo['forks'], repo['commits_url'][:-6]])
+        
+        for commits in reporesults[:commites]:
+            print(commits[-1],"commits")
+        
+            commit_urls=commits[-1]
+            commit_data=json.loads((requests.get(commit_urls).text))
+           
+            personlist=[]
+            commitcount=[]
+            
+        
+            for person in commit_data:
+                if person['committer']['id'] in personlist:
+    
+                    index = personlist.index(person['committer']['id'])
+                    
+                    count = commitcount[index] +1
+                               
+                    commitcount[index]=count
+                else:
+                   
+                    personlist.append(person['committer']['id'])
+                    commitcount.append(1)
+            
+                
+            result = [[person['committer']['id'] for person in commit_data] ]
+            
+            
+            print("Committee ID", personlist)
+            print("Committee count", commitcount)
 
+        return Response ({"message":"success", "data":commit_data})
+
+    
 # {"forks":"8"}
 # https://api.github.com/search/repositories?q=forks:%3C50+sort:forks-desc
